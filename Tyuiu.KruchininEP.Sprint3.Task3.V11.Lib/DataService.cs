769d12ee33cf8cc40b60e05d30490cbc2a39bc68
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-10-31 19:58:48 +0500

    Task3

---
+++ Tyuiu.KruchininEP.Sprint3.Task3.V11.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+using Tyuiu.KruchininEP.Sprint3.Task3.V11.Lib;
+
+namespace Tyuiu.KruchininEP.Sprint3.Task3.V11.Lib
+{
+    public class DataService : ISprint3Task3V11
+    {
+        public string ReplaceCharOnNum(string value, char replaceable, char replacement)
+        {
+            foreach (char c in value)
+            {
+                if (c == replaceable)
+                {
+                    value = value.Replace(replaceable, replacement);
+                }
+            }
+
+            return value;
+        }
+    }
+}
